9ecf3c79ec9ea563cf3e2e695ae34c07880f7d9e
Sandadi Rithvik Reddy <srreddy@EX2INDIA.LOCAL>
2024-06-14 16:40:29 +0530

    first-commi

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/models/dummy_data.dart';
+import 'package:food_app/widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick a Category'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(15),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
+        ],
+      ),
+    );
+  }
+}
